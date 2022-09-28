9712bf4a5278ce07746db91a1894e6324bb4308d
Michael <michaeljoseph6950@gmail.com>
2022-09-29 01:25:50 +0300

    ch

--- public/js/main/index.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    var pageSection = $(".parallax,.bg-img");
-    pageSection.each(function(indx) {
-        if ($(this).attr("data-background")) {
-            $(this).css("background-image", "url(" + $(this).data("background") + ")");
-        }
-    });
-}
-)(jQuery);
